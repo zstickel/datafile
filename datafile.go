86c8d0e455d96edca70b425b2fd6db2727257444
zstickel <63936086+zstickel@users.noreply.github.com>
2021-10-02 20:51:44 -0400

    with package

---
+++ datafile.go
@@ -0,0 +1,34 @@
+ackage datafile
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
+
+//GetFloats reads a float64 from each line of a File and returns an array of float64s.
+
+func GetFloats(fileName string) ([3]float64, error) {
+	var numbers [3]float64
+	file, err := os.Open(fileName)
+	if err != nil {
+		return numbers, err
+	}
+	i := 0
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		numbers[i], err = strconv.ParseFloat(scanner.Text(), 64)
+		if err != nil {
+			return numbers, err
+		}
+		i++
+	}
+	err = file.Close()
+	if err != nil {
+		return numbers, err
+	}
+	if scanner.Err() != nil {
+		return numbers, scanner.Err()
+	}
+	return numbers, nil
+}
